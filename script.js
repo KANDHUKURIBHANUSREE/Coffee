ebe2460d81c2f1228a19b7c4630b76846af34dfa
KANDHUKURIBHANUSREE <kandhukuribhanusree@gmail.com>
2025-03-20 15:57:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const orderButton = document.querySelector('.btn');
+
+    orderButton.addEventListener('click', () => {
+        alert("Redirecting you to our 'Order Now' page. Enjoy your coffee journey!");
+    });
+});
